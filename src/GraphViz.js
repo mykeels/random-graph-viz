c23ef7b4bafac52f730e5b99ae6a304da2f7177a
Ikechi Michael <mykehell123@gmail.com>
2020-05-27 13:47:23 +0100

    feat: regenerate and export to console

--- src/GraphViz.js
+++ src/GraphViz.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Graph } from "react-d3-graph";
 import generate from "random-graph-generator";
 
@@ -14,28 +14,60 @@ const config = {
   },
 };
 
-const data = generate(
-  new Array(50).fill(null).map((_, i) => i.toString()),
-  {
-    maxWidth: 4,
-    edgeProbability: 0.3,
-  }
-).serialize();
+const regenerate = () => {
+  const data = generate(
+    new Array(50).fill(null).map((_, i) => i.toString()),
+    {
+      maxWidth: 5,
+      edgeProbability: 0.3,
+    }
+  ).serialize();
+  
+  const refinedNodes = {
+    ...data,
+    nodes: data.nodes.map((node, i) => ({
+      ...node,
+      ...(Math.random() < 0.1 ? { color: "black" } : {}),
+    })),
+  };
 
-const refinedNodes = {
-  ...data,
-  nodes: data.nodes.map((node, i) => ({
-    ...node,
-    ...(Math.random() < 0.1 ? { color: "black" } : {}),
-  })),
-};
+  return refinedNodes;
+}
 
-export default function () {
+
+
+export default () => {
+  const [nodes, setNodes] = useState(regenerate());
+
+  const exportGraphson = () => {
+    console.log(
+      JSON.stringify(
+        nodes,
+        null,
+        2
+      )
+    );
+  };
+
   return (
-    <Graph
-      id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
-      data={refinedNodes}
-      config={config}
-    />
+    <>
+      <button 
+        type="button" 
+        style={{ position: 'fixed', top: '10px', right: '10px', fontSize: '20px', padding: '0.75rem' }}
+        onClick={() => setNodes(regenerate())}>
+        Generate new Graph
+      </button>
+      <button 
+        type="button" 
+        style={{ position: 'fixed', top: '70px', right: '10px', fontSize: '20px', padding: '0.75rem' }}
+        onClick={exportGraphson}>
+        Export to Console
+      </button>
+      <Graph
+        id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+        data={nodes}
+        config={config}
+      />
+    </>
   );
 }
