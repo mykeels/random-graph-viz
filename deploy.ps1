6c7fa0e272bc51dad45e23a38f54ad6d20189804
mykeels <mykehell123@gmail.com>
2020-06-07 02:31:24 +0100

    feat: try again

--- deploy.ps1
+++ deploy.ps1
@@ -4,18 +4,12 @@ param (
 
 "Deleting old buildation"
 Remove-Item .\build -Force -Recurse -ErrorAction Ignore
-mkdir build | out-null
+yarn build
 git worktree prune
 Remove-Item .git\worktrees\build\ -Force -Recurse -ErrorAction Ignore
 
 "Checking out gh-pages branch into build"
 git worktree add -B gh-pages build origin/gh-pages
-
-"Removing existing files"
-Remove-Item .\build\* -Force -Recurse -Exclude '.git'
-
-"Generating site"
-hugo --quiet
 
 "Committing master branch"
 git add --all
@@ -24,10 +18,7 @@ git commit -m $commitMessage --allow-empty
 "Committing gh-pages branch"
 Push-Location -path build
 git add --all
-git commit -m $commitMessage
-
-"Pushing master to Github"
-git push origin master
+git commit -m $commitMessage --allow-empty
 
 "Pushing gh-pages branch to Github"
 git push origin gh-pages
