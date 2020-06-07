859ff976cbe5a77456758d9a724864ca596338d7
mykeels <mykehell123@gmail.com>
2020-06-07 01:57:53 +0100

    feat: enhance for tree|graph, specify treasure count

--- src/GraphViz.js
+++ src/GraphViz.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Graph } from "react-d3-graph";
-import generate from "random-graph-generator";
+import { generateRandomTree, generateRandomGraph } from "random-graph-generator";
 
 const config = {
   nodeHighlightBehavior: true,
@@ -12,26 +12,32 @@ const config = {
   link: {
     highlightColor: "red",
   },
+  automaticRearrangeAfterDropNode: true,
 };
 
-const regenerate = ({ maxWidth, count }) => {
-  const data = generate(
+const regenerate = ({ maxWidth, count, blackCount, graph }) => {
+  const fn = graph ? generateRandomGraph : generateRandomTree;
+  const data = fn(
     new Array(count).fill(null).map((_, i) => i.toString()),
     {
       maxWidth,
       edgeProbability: 0.3,
     }
   ).serialize();
-  
-  const refinedNodes = {
-    ...data,
-    nodes: data.nodes.map((node, i) => ({
-      ...node,
-      ...(Math.random() < 0.1 ? { color: "black" } : {}),
-    })),
-  };
 
-  return refinedNodes;
+  let convertedCount = 0;
+
+  while (convertedCount < blackCount) {
+    for (let i = 0; i < data.nodes.length; i++) {
+      if (Math.random() < (1 / data.nodes.length * blackCount)) {
+        data.nodes[i].color = 'black';
+        convertedCount++;
+      }
+      if (convertedCount >= blackCount) break;
+    }
+  }
+
+  return data;
 }
 
 
@@ -39,6 +45,7 @@ const regenerate = ({ maxWidth, count }) => {
 export default () => {
   const [count, setCount] = useState(50);
   const [maxWidth, setMaxWidth] = useState(5);
+  const [blackCount, setBlackCount] = useState(5);
   const [nodes, setNodes] = useState(regenerate({ count, maxWidth }));
 
   const exportGraphson = () => {
@@ -67,15 +74,28 @@ export default () => {
           inputMode="numeric" onChange={e => setMaxWidth(Number(e.target.value))}
           style={{ fontSize: '14px', padding: '0.5rem', width: '50px' }} />
       </div>
+      <div
+        style={{ position: 'fixed', top: '90px', right: '10px', fontSize: '14px', padding: '0.75rem', fontWeight: 'bold' }}>
+        <label>Black Nodes:</label>
+        <input type="number" min="1" value={blackCount} 
+          inputMode="numeric" onChange={e => setBlackCount(Number(e.target.value))}
+          style={{ fontSize: '14px', padding: '0.5rem', width: '50px' }} />
+      </div>
       <button 
         type="button" 
-        style={{ position: 'fixed', top: '110px', right: '10px', fontSize: '14px', padding: '0.75rem' }}
-        onClick={() => setNodes(regenerate({ count, maxWidth }))}>
-        Regenerate
+        style={{ position: 'fixed', top: '160px', right: '10px', fontSize: '14px', padding: '0.75rem' }}
+        onClick={() => setNodes(regenerate({ count, maxWidth, graph: false, blackCount }))}>
+        Generate Tree
       </button>
       <button 
         type="button" 
-        style={{ position: 'fixed', top: '170px', right: '10px', fontSize: '14px', padding: '0.75rem' }}
+        style={{ position: 'fixed', top: '230px', right: '10px', fontSize: '14px', padding: '0.75rem' }}
+        onClick={() => setNodes(regenerate({ count, maxWidth, graph: true, blackCount }))}>
+        Generate Graph
+      </button>
+      <button 
+        type="button" 
+        style={{ position: 'fixed', top: '300px', right: '10px', fontSize: '14px', padding: '0.75rem' }}
         onClick={exportGraphson}>
         Export to Console
       </button>
